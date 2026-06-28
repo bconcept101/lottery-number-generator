072ede67d82056c2b4eb59f1bc5217b75ac30360
bconcept101 <bconcept101@gmail.com>
2026-06-27 22:38:04 -0400

    Create database.js

---
+++ database.js
@@ -0,0 +1,38 @@
+const pastNumbers = {
+  powerball: [
+    {
+      drawDate: "Sample",
+      numbers: [12, 18, 24, 36, 45],
+      powerball: 7
+    },
+    {
+      drawDate: "Sample",
+      numbers: [3, 15, 27, 41, 62],
+      powerball: 19
+    }
+  ],
+
+  mega: [
+    {
+      drawDate: "Sample",
+      numbers: [8, 14, 22, 39, 50],
+      megaBall: 12
+    },
+    {
+      drawDate: "Sample",
+      numbers: [5, 17, 31, 44, 66],
+      megaBall: 20
+    }
+  ],
+
+  pick5: [
+    {
+      drawDate: "Sample",
+      numbers: [1, 2, 3, 4, 5]
+    },
+    {
+      drawDate: "Sample",
+      numbers: [7, 4, 9, 0, 2]
+    }
+  ]
+};
